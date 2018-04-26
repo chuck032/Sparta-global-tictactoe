1fb1dd3499fd8427f8f150e299e9e0331d780eb8
Tech-A38 <tech-a38@Tech-A38s-MacBook-Air.local>
2018-04-27 00:55:24 +0100

    completed tictactoe

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # Sparta-global-tictactoe
+
+JQuery Selectors and Events - TicTacToe
+
+You challenge is build the game TicTacToe in the browser using JQuery. We will give you the html layout for the game. We will need you to use what you have learnt so far with selecting things with JQuery and creating event listeners with JQuery to create this game.
+
+Your deliverables:-
+
+1) Git repo with Dev and Master branches
+2) Finished README.md with brief, what went well, the challenges and list of what you delivered
+3) pull-request submitted by 9:00am tomorrow
+4) The game should display the correct 'X' or 'Y' for each button pressed in the browser
+5) Once a tile has been clicked it should not be able to be clicked again
+6) The clear button should clear the board
+7) When a winning condition is present you should display and winner, wither X or Y
+8) The program should use functions, loops, JQuery selectors and JQuery events
--- script.js
+++ script.js
@@ -1,16 +1,130 @@
-var appear = $("[data-num]");
-
-// .click(function(){
 //
-// });
+// var sq1 = $("#0")
+// var sq2 = $("#1")
+// var sq3 = $("#2")
+// var sq4 = $("#3")
+// var sq5 = $("#4")
+// var sq6 = $("#5")
+// var sq7 = $("#6")
+// var sq8 = $("#7")
+// var sq9 = $("#8")
+// var space = [ sq1,sq2,sq3,sq4,sq5,sq6,sq7,sq8,sq9];
+// var winning = []
+// var player = 0;
+// var comp = 1;
+// var turn = 1;
+//
+// var appear = $("[data-num]");
+//
+// $("#reset").click(function(){
+//   $("[data-num]").removeClass("X").removeClass("O").html("");
+// })
+//
+//
+// // function game(){
+// //   for (var i = 0; i < 9, i++) {
+// //
+// //   if (turn  === 0 ){
+// //     $("td").click(function(event){
+// //       $(this).addClass("X").html("X");
+// //     })
+// //     return turn = 1 ;
+// //   } if (turn === 1 ){
+// //     var spaces = space[(Math.floor(Math.random() * Math.floor(9)))];
+// //     $(spaces).addClass("O").html("O");
+// //     return turn = 0;
+// //   }
+// //   }
+// // }
+// //
+// // game();
+//
 
 
-function crosses(){
-var cross = $(appear).addClass("X").html("X");
-return cross;
+
+$(document).ready(function() {
+
+var sq1 = $('data-num' | '0');
+var sq2 = $('data-num' | '1');
+var sq3 = $('data-num' | '2');
+var sq4 = $('data-num' | '3');
+var sq5 = $('data-num' | '4');
+var sq6 = $('data-num' | '5');
+var sq7 = $('data-num' | '6');
+var sq8 = $('data-num' | '7');
+var sq9 = $('data-num' | '8');
+
+
+
+var checkPlay = false;
+
+
+
+
+function validatePlay(squareplayed) {
+	if ( $(squareplayed).hasClass('empty') ) {
+		checkPlay = true;
+	} else {
+		checkPlay = false;
+		return false;
+	}
 }
 
-function noughts() {
-var nought = $(appear).addClass("O").html("O");
-return nought;
+function clearBoard() {
+	$('.square').removeClass('O');
+	$('.square').removeClass('X');
+	$('.square').html('');
+	$('.square').addClass('empty');
 }
+
+
+
+
+var count = 0;
+
+$('.square').on('click', function play() {
+ count++;
+
+	validatePlay(this);
+
+	if (checkPlay && (count % 2)) {
+		$(this).removeClass('empty');
+		$(this).addClass('X');
+		$(this).html("X");
+    checkWin()
+
+	}
+	else if (checkPlay && !(count % 2)){
+
+		$(this).removeClass('empty');
+		$(this).addClass('O');
+		$(this).html("O");
+    checkWin()
+		}
+
+		else {
+		alert("That square has already been played. Please choose another square");
+	}
+
+});
+
+function checkWin(){
+  if ($(sq1).text()==$(sq2).text()==$(sq3).text() || $(sq1).text()==$(sq4).text()==$(sq7).text() || $(sq1).text()==$(sq5).text()==$(sq9).text()){
+   alert($(sq1).text() + "has won")
+  }if ($(sq2).text()==$(sq5).text()==$(sq8).text()){
+   alert($(sq2).text() + "has won")
+  }if ($(sq3).text()==$(sq6).text()==$(sq9).text() || $(sq3).text()==$(sq5).text()==$(sq7).text()){
+   alert($(sq3).text() + "has won")
+  }if ($(sq4).text()==$(sq5).text()==$(sq6).text()){
+   alert($(sq4).text() + "has won")
+  }if ($(sq7).text()==$(sq8).text()==$(sq9).text()){
+   alert($(sq7).text() + "has won")
+  }
+}
+
+$('#reset').on('click', function() {
+	clearBoard();
+})
+
+
+});
